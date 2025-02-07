7d3ed227d9c317eb6baed3f0ffa00a28ffa4e464
JangBJ <qudwnd1001@naver.com>
2025-02-07 10:15:54 +0900

    2 problem

---
+++ 002-문자열 뒤집기/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        int l = s.length;
+        int len = s.length/2;
+
+        for(int i=0; i<len; i++){
+            char tmp = s[l-(i+1)];
+            s[l-(i+1)] = s[i];
+            s[i] = tmp;
+        }
+    }
+}
